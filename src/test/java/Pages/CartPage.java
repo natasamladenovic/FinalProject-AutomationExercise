50b386be6f5fc1a70dbdf6c1f22dd059faaa4023
Natasa Mladenovic <natasa26dec@gmail.com>
2026-06-01 20:51:59 +0200

    Implemented checkout funcionality

--- src/test/java/Pages/CartPage.java
+++ src/test/java/Pages/CartPage.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -24,10 +25,88 @@ public class CartPage {
         return driver.findElement(By.xpath("//p[@class='text-center']/b"));
     }
 
+    public WebElement getProceedToCheckoutButton() {
+        return driver.findElement(By.xpath("/html/body/section/div/section/div[1]/div/div/a"));
+    }
+
+    public WebElement getPlaceOrderButton() {
+        return driver.findElement(By.xpath("/html/body/section/div/div[7]/a"));
+    }
+
+    public WebElement getNameOnCardField() {
+        return driver.findElement(By.name("name_on_card"));
+    }
+
+    public WebElement getCardNumberField() {
+        return driver.findElement(By.name("card_number"));
+    }
+
+    public WebElement getCvcField() {
+        return driver.findElement(By.name("cvc"));
+    }
+
+    public WebElement getExpirationMonthField() {
+        return driver.findElement(By.name("expiry_month"));
+    }
+
+    public WebElement getExpirationYearField() {
+        return driver.findElement(By.name("expiry_year"));
+    }
+
+    public WebElement getPayAndConfirmOrder() {
+        return driver.findElement(By.id("submit"));
+    }
+
+    public WebElement getOrderPlacedMessage() {
+        return driver.findElement(By.xpath("/html/body/section/div/div/div/h2/b"));
+    }
+
 
 
 
     public void clickRemoveItemButton() {
         getRemoveItem().click();
     }
+
+    public void clickProceedToCheckoutButton() {
+        getProceedToCheckoutButton().click();
+    }
+
+    public void clickPlaceOrderButton() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",
+                getPlaceOrderButton());
+
+        getPlaceOrderButton().click();
+    }
+
+    public void inputNameOnCard(String name) {
+        getNameOnCardField().clear();
+        getNameOnCardField().sendKeys(name);
+    }
+
+    public void inputCardNumber(String cardNumber) {
+        getCardNumberField().clear();
+        getCardNumberField().sendKeys(cardNumber);
+    }
+
+    public void inputCvc(String cvc) {
+        getCvcField().clear();
+        getCvcField().sendKeys(cvc);
+    }
+
+    public void inputExpirationMonth(String month) {
+        getExpirationMonthField().clear();
+        getExpirationMonthField().sendKeys(month);
+    }
+
+    public void inputExpirationYear(String year) {
+        getExpirationYearField().clear();
+        getExpirationYearField().sendKeys(year);
+    }
+
+    public void clickPayAndConfirmOrderButton() {
+        getPayAndConfirmOrder().click();
+    }
+
 }
--- src/test/java/Tests/CartTest.java
+++ src/test/java/Tests/CartTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class CartTest extends BaseTest {
 
-    @Test
+    @Test (priority = 20)
     public void userCanAddOneItemToCart() {
         homePage.clickProductsButton();
         productsPage.inputProductName("Blue Top");
@@ -17,7 +17,7 @@ public class CartTest extends BaseTest {
         Assert.assertEquals(cartPage.getProductInCart().getText(), "Blue Top");
     }
 
-    @Test
+    @Test (priority = 30)
     public void userCanAddMoreThanOneItemToCart() {
         homePage.clickProductsButton();
         productsPage.inputProductName("Sleeveless Dress");
@@ -27,8 +27,12 @@ public class CartTest extends BaseTest {
         productsPage.clickViewCartButton();
     }
 
-    @Test
+    @Test (priority = 10)
     public void userCanRemoveItemFromTheCart() throws InterruptedException {
+        homePage.clickOnSignupLogin();
+        loginPage.inputLoginEmail("janedoeqa26@gmail.com");
+        loginPage.inputLoginPassword("Testqa91!");
+        loginPage.clickLoginButton();
         homePage.clickProductsButton();
         productsPage.inputProductName("Sleeveless Dress");
         productsPage.clickSearchButton();
@@ -39,5 +43,21 @@ public class CartTest extends BaseTest {
         Thread.sleep(2000);
         Assert.assertTrue(cartPage.getEmptyCartMessage().isDisplayed());
     }
+
+    @Test (priority = 40)
+    public void userCanProceedToCheckout() throws InterruptedException {
+        Thread.sleep(2000);
+        cartPage.clickProceedToCheckoutButton();
+        cartPage.clickPlaceOrderButton();
+        cartPage.inputNameOnCard("Jane Doe");
+        cartPage.inputCardNumber("1234 5678 9101 2345");
+        cartPage.inputCvc("629");
+        cartPage.inputExpirationMonth("08");
+        cartPage.inputExpirationYear("2028");
+        cartPage.clickPayAndConfirmOrderButton();
+        Assert.assertTrue(cartPage.getOrderPlacedMessage().isDisplayed());
+    }
+
+
 
 }
